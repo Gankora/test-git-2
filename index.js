c2f059eec4e147e24ad004ad9f76fab36165c049
Kostya Kryukaev <kryukaev.kostya@yandex.ru>
2024-08-16 19:57:49 +0300

    changed feature/user-access

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('Hello world');
 
-const age = 22;
-if (age >= 18) {
+const userAge = 42;
+if (userAge >= 18) {
     console.log('Вы совершеннолетний');    
 } else {
     console.log('Вы НЕ совершеннолетний');
